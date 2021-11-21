0d9c3c75a67d5c6972153b5255ed5e730e3724b9
unknown <ewereeman@gmail.com>
2021-11-20 21:49:47 -0500

    Story 1 task- Design screen layout for lawn tennis trainer seeker registration page

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Secret = exports.HostName = exports.LocalURI = void 0;
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://127.0.0.1:27017";
+exports.RemoteURI = "mongodb+srv://admin:L650Xs9ixkF3fwGr@cluster0.nk3nn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 exports.HostName = "RemoteHost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
